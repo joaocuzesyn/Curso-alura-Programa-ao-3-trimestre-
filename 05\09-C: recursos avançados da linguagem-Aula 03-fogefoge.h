afb9bd3017fa8e6908a42ffbff737b9b0242decd
joaocuzesyn <164204017+joaocuzesyn@users.noreply.github.com>
2025-09-19 11:16:12 -0300

    05\09-C: recursos avançados da linguagem-Aula 03-fogefoge.h

---
+++ 05\09-C: recursos avançados da linguagem-Aula 03-fogefoge.h
@@ -0,0 +1,12 @@
+#codigo feito te aula 03 dia 05\09-fogefoge.h
+
+#define CIMA 'w'
+#define BAIXO 's'
+#define DIREITA 'd'
+#define ESQUERDA 'a'
+
+int acabou();
+void move(char direcao);
+ 
+int ehdirecao(char direcao);
+void fantasmas();
